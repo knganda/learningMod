befde414238b3c3fa0b552813848446bec02f5a4
knganda <knganda@outlook.com>
2024-02-19 17:16:11 +0300

    initial commit

--- number.py
+++ number.py
@@ -1,10 +1,26 @@
-while True:
-    try:
-        x = int(input("What's x? "))    
-    except ValueError:
-        continue
-        #print("x is not an integer") 
-    else:
-        break
-        
-print(f"x is {x}")
+def main():
+    x = get_int("What's x? ")
+    print(f"The number entered is {x}")
+
+
+def get_int(prompt):
+    while True:
+        try:
+            x = int(input(prompt))   
+            break 
+            # return int(input("What's x? "))
+        except ValueError:
+            #continue
+            #print("x is not an integer") 
+            pass
+        # else:
+        #     break
+    return x
+
+if __name__=="__main__":
+    main()
+    
+
+    
+    
+

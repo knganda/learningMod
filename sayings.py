8dcb91432688b448789259d7dde9dbd9a69d3936
knganda <knganda@outlook.com>
2024-02-20 14:56:09 +0300

    intial commit

---
+++ sayings.py
@@ -0,0 +1,17 @@
+def main():
+    hello("Kevin")
+    goodbye("Kevine")
+
+
+def hello(name):
+    print(f"Hello, {name}")
+    
+def goodbye(name):
+    print(f"Goodbye, {name}")
+    
+    
+    
+if __name__ == "__main__":
+    main()
+
+

394b4ec2e8e7537b1dda32aaa5bc4be9c2b34a94
knganda <knganda@outlook.com>
2024-02-20 14:56:35 +0300

    intial commit

--- itunes.py
+++ itunes.py
@@ -1,15 +1,21 @@
 import requests
 import sys
+import json
 
 
 # Appending sys.argv to the url enables the user to specify a different band via the terminal
 
-url = "https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.orig_argv[1]
+url = "https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1]
 
 if len(sys.argv) != 2:
     sys.exit()
 
 response = requests.get(url)
 
-print(response.json())
+#print(json.dumps(response.json(), indent=2))
+o = response.json()
+
+for result in o['results']:
+    print(result['trackName'])
+    
 
